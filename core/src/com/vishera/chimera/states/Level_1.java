36157474548694eef25fa1e1ba9ece0b78dff7fa
Nathaniel Yost <nathanyost15@gmail.com>
2020-05-05 00:06:08 -0400

    Add MainMenu/Level_1 states

---
+++ core/src/com/vishera/chimera/states/Level_1.java
@@ -0,0 +1,20 @@
+package com.vishera.chimera.states;
+
+import com.vishera.chimera.gsm.GameState;
+import com.vishera.chimera.gsm.GameStateManager;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Level_1 extends GameState {
+    public Level_1(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void render() {
+    }
+
+    @Override
+    public void update() {
+    }
+}
